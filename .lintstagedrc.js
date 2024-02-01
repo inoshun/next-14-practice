793d5ddb6faf05a59ab0750b62f6047df47d9a90
ShunInoue <shun.inoue11@gmail.com>
2024-02-02 03:17:08 +0900

    fix lint-staged commands

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,7 +3,9 @@ const path = require('path')
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
 
+const prettierCommand = 'prettier --write'
+
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
-  '*.{js,jsx,ts,tsx,html,json,css}': 'prettier --write',
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, prettierCommand],
+  '*.{html,json,css}': prettierCommand,
 }
