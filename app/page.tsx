fe63bdfadb468262cb4d7bb5f211dfe007ec583d
ShunInoue <shun.inoue11@gmail.com>
2024-02-02 16:54:40 +0900

    "fix top page title"

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <p>My next 14 project.</p>
+      <h1>Next14 practice</h1>
     </main>
   )
 }
