b11c56c8462e6540695749e227c1e9b7b256d111
Shun Inoue <shun.inoue11@gmail.com>
2024-02-02 18:21:43 +0900

    fix top page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Next14 Practice</h1>
+      <h1>Next14 Practice App</h1>
       <p>This is a demo site for practicing Next.js 14.</p>
     </main>
   )
