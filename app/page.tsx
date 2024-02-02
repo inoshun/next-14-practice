1dd5bf1b490608c9c620a7a399da6797e2fcb2be
Shun Inoue <shun.inoue11@gmail.com>
2024-02-02 17:53:59 +0900

    fix explanation text to top page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Next14 Practice</h1>
-      <p>This is a demo site for practicing Next14.</p>
+      <p>This is a demo site for practicing Next.js 14.</p>
     </main>
   )
 }
