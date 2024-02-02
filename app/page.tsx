53bdbbdf4170f20ee2ba69ed4f316f1aac7ab289
Shun Inoue <shun.inoue11@gmail.com>
2024-02-02 17:50:26 +0900

    "add explanation text to top page"

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Next14 practice</h1>
+      <h1>Next14 Practice</h1>
+      <p>This is a demo site for practicing Next14.</p>
     </main>
   )
 }
