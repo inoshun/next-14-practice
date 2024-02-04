f33c821dd6b01f0593fbcdfe79627f2458a7ce15
Shun Inoue <shun.inoue11@gmail.com>
2024-02-05 01:30:53 +0900

    update top page explanation

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Next14 Practice App</h1>
-      <p>This is a demo site for practicing Next.js 14</p>
+      <p>This is a demo site for practicing Next.js 14 and React 18.</p>
     </main>
   )
 }
