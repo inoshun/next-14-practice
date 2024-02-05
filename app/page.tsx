deb5266cbf5ee73ecddc8c60f22dbb5bd0a94118
inoshun <33561283+inoshun@users.noreply.github.com>
2024-02-05 13:52:57 +0900

    Update page.tsx description version

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Next v14 Practice App</h1>
-      <p>This is a demo site for practicing Next.js v14 and React v18.</p>
+      <p>This is a demo site for practicing Next.js version 14 and React version 18.</p>
     </main>
   )
 }
