c6391da33d18029a6b1fd6bbc1798f7473244869
Shun Inoue <shun.inoue11@gmail.com>
2024-02-05 20:22:42 +0900

    Fix top page description 'for practicing' to 'to practice'

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Next.js version 14 Practice App</h1>
-      <p>This is a demo site for practicing Next.js v-14 and React v-18.</p>
+      <p>This is a demo site to practice Next.js v-14 and React v-18.</p>
     </main>
   )
 }
